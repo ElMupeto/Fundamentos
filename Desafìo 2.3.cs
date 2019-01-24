95dea9eb9f28b1b15b1a36106b38ca3fb8583b1f
ElMupeto <47011726+ElMupeto@users.noreply.github.com>
2019-01-24 18:43:31 -0500

    Add files via upload

---
+++ Desafìo 2.3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un ángulo c y el valor del cateto1");
+            double cgrados = double.Parse(Console.ReadLine());
+            double z = double.Parse(Console.ReadLine());
+            double c = cgrados * (Math.PI / 180.0);
+            Console.WriteLine("Para un triángulo rectángulo los datos resultantes serían");
+            Console.WriteLine("Sen(c) = z/h asi que h = z/sen(c)");
+            double h = z / Math.Sin(c);
+            Console.WriteLine("Hipotenusa =" + h);
+            Console.WriteLine("Cos(c) = y/h asi que y = cos(c)*h");
+            double y = Math.Cos(c) * h;
+            Console.WriteLine("cateto2 =" + y);
+            Console.WriteLine("Sen(a) = y/h asi que a = arcsen(y/h)");
+            double a = Math.Asin(y / h) * (180.0 / Math.PI);
+            Console.WriteLine("ángulo a =" + a);
+            Console.WriteLine("ángulo b =90");
+
+        }
+    }
+}
---
+++ Desafío 2.1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese cateto1 Y cateto2");
+            double z = double.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+            Console.WriteLine("Para un triángulo rectángulo los datos resultantes serían");
+            double h = Math.Sqrt((y * y) + (z * z));
+            Console.WriteLine("Hipotenusa =" + h);
+            Console.WriteLine("Sen(a) = z/h asi que a = arcseno(z/h)");
+            double a = Math.Asin(z / h) * (180.0 / Math.PI);
+            Console.WriteLine("ángulo a =" + a);
+            Console.WriteLine("Sen(a) = y/h asi que a = arcseno(y/h)");
+            double c = Math.Asin(y / h) * (180.0 / Math.PI);
+            Console.WriteLine("ángulo c =" + c);
+            Console.WriteLine("ángulo b =90");
+        }
+    }
+}

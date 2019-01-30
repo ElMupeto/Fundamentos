e7d43658b5e406d9133a61e481c653b63580d1cf
ElMupeto <47011726+ElMupeto@users.noreply.github.com>
2019-01-29 23:15:44 -0500

    Add files via upload

---
+++ Desafío 4.2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ingreso de Ángulo d, Ángulo b & el cateto y
+            Console.WriteLine("Ingrese Angulo d & Ángulo b & Cateto y ");
+            double dgrados = double.Parse(Console.ReadLine());
+            double bgrados = double.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+
+            //Transformación de grados a rads
+            double d = dgrados * (Math.PI / 180.0);
+            double b = bgrados * (Math.PI / 180.0);
+
+
+            //Hallamos a ya que tenemos 2 ángulos principales y 1 complementario del trángulo mayor
+            double agrados = (180.0 - bgrados - dgrados - 90.0);
+            double a = agrados * (Math.PI / 180.0);
+            Console.WriteLine("Ángulo a =" + agrados);
+
+            //Usamos tan(a) = y/z y despejamos z
+            double z = (y / Math.Tan(a));
+            Console.WriteLine("Cateto z =" + z);
+
+        }
+    }
+}
---
+++ Desafío 4.4.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poryecto_4._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ingreso de Ángulo d, Ángulo b & el cateto y
+            Console.WriteLine("Ingrese Ángulo c & Cateto w & Cateto t ");
+            double cgrados = double.Parse(Console.ReadLine());
+            double w = double.Parse(Console.ReadLine());
+            double t = double.Parse(Console.ReadLine());
+
+            //Transformación de grados a rads
+            double c = cgrados * (Math.PI / 180.0);
+
+            //Hallamos e por medio de su ángulo complementario conocido
+            double egrados = 180.0 - cgrados;
+            double e = egrados * (Math.PI / 180.0);
+            Console.WriteLine("Ángulo e= " + egrados);
+
+            //Usamos ley de seno y despejamos d
+
+            double d = Math.Asin((Math.Sin(e) * t) / w);
+            double dgrados = d * (180.0 / Math.PI);
+            Console.WriteLine("d =" + dgrados);
+
+            //Hallamos b restando a 180 los ángulos conocidos
+
+            double bgrados = 180.0 - dgrados - egrados;
+            double b = bgrados * (Math.PI / 180.0);
+            Console.WriteLine("b =" + bgrados);
+
+            //Usamos ley de seno y hallamos x
+            double x = (Math.Sin(b) * t) / Math.Sin(d);
+            Console.WriteLine("x =" + x);
+
+
+
+
+        }
+    }
+}
